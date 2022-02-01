878cb18c2029b3862d0bdc97e4045da440744356
nelu <unzel@unzel.com>
2022-02-01 20:36:35 +0200

    fullpath in when creating a share 404 method view

---
+++ src/ShellCmds.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace Flm;
+
+
+class ShellCmds
+{
+    public static function mkdir($target, $recursive = false, $mode = null)
+    {
+        $args = ['mkdir'];
+
+        if($mode != null)
+        {
+            $args[] = '--mode=' . $mode;
+        }
+        if ($recursive) {
+            $args[] = '-p';
+        }
+
+        $args[] = Helper::mb_escapeshellarg($target);;
+
+        return $args;
+    }
+
+    public static function recursiveCopy($source, $to)
+    {
+        $fName = Helper::mb_escapeshellarg('✓ ' . basename($source));
+        $source = Helper::mb_escapeshellarg($source);
+        $to = Helper::mb_escapeshellarg($to);
+
+        return "cp -rpv {$source} {$to} && echo {$fName}";
+    }
+
+    public static function recursiveMove($file, $to): string
+    {
+        $fName = Helper::mb_escapeshellarg('✓ ' . basename($file));
+        $file = Helper::mb_escapeshellarg($file);
+        $to = Helper::mb_escapeshellarg($to);
+
+        return "mv -f ${file} ${to} && echo {$fName}";
+    }
+
+    public static function recursiveRemove($file): string
+    {
+        $fName = Helper::mb_escapeshellarg('✓ ' .basename($file));
+        $file = Helper::mb_escapeshellarg($file);
+        $cmd = "rm -rf {$file} && echo {$fName}";
+
+        return $cmd;
+    }
+}
