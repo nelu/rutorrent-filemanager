fce1c75fc7b4daf0bf5cf9c679cb674ac898e85d
nelu <unzel@unzel.com>
2022-02-03 17:04:38 +0200

    disabled strict error checking - related to https://github.com/Novik/ruTorrent/commit/3becce06c5ec3be40e97917e0362a0ffd525377c

--- boot.php
+++ boot.php
@@ -1,9 +1,9 @@
 <?php
 namespace Flm;
 require_once (__DIR__ . '/../_task/task.php');
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 require_once (__DIR__ . '/src/Helper.php');
 Helper::registerAutoload();
