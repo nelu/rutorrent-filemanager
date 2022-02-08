f8e2769b4c0b84a86c6860c802130ca231793345
hwk <loool@lool.om>
2022-02-09 01:45:58 +0200

    hash tests

--- test.php
+++ test.php
@@ -1,51 +1,62 @@
 <?php
-if(!defined('STDOUT'))
+
+use Flm\Archive;
+use Flm\P7zip;
+use Flm\Rar;
+use Flm\ShellCmd;
+use Flm\ShellCmds;
+
+if (!defined('STDOUT'))
 {
     exit;
 }
 
 include 'boot.php';
+$result = P7zip::hash('/data/downloads/Hola/1test.mp4');
+
+var_dump($result->cmd(), $result->run());
 
 var_dump(
-\Flm\ShellCmds::mkdir('/tmp/newdir', true, '0777')
 
+    ShellCmds::mkdir('/tmp/newdir', true, '0777'),
+    ShellCmd::bin('/bin/ls', ['-la', '/tmp/'])->end('|')->setArgs(['cat'])->run()
 );
 
 //exit;
 var_dump(
-    \Flm\Rar::unpack('myarchive.rar', '/tmp/')->setPassword("123")->cmd(),
-    \Flm\Rar::pack('myarchive.rar')
+    Rar::unpack('myarchive.rar', '/tmp/')->setPassword("123")->cmd(),
+    Rar::pack('myarchive.rar')
         ->setFileList('/tmp/filelist.txt')
         ->cmd(),
     // zip file
-    \Flm\Archive::compressCmd((object)[
+    Archive::compressCmd((object)[
         'type' => 'zip',
         'archive' => './mydir/myarchive.zip',
         'binary' => '7z',
         'fileList' => '/tmp/myarchive.zip_compress_list.txt',
         'password' => "123",
-        'volumeSize' => 1024*1000,
+        'volumeSize' => 1024 * 1000,
         'compression' => 0
     ]),
     // tar.gz file
-    \Flm\Archive::compressCmd((object)[
+    Archive::compressCmd((object)[
         'type' => 'tar.gz',
         'multiplePass' => ['tar', 'gzip'],
         'archive' => './mydir/myarchive.tgz',
         'binary' => '7z',
         'fileList' => '/tmp/myarchive.tgz_compress_list.txt',
-       // 'password' => "123", // not supported
-        'volumeSize' => 1024*1000,
+        // 'password' => "123", // not supported
+        'volumeSize' => 1024 * 1000,
         'compression' => 0
     ]),
     // rar file
-    \Flm\Archive::compressCmd((object)[
+    Archive::compressCmd((object)[
         'type' => 'rar',
         'archive' => './mydir/myarchive.rar',
         'binary' => '/home/user/bin/rar',
         'fileList' => '/tmp/myarchive.rar_compress_list.txt',
         'password' => "123",
-        'volumeSize' => 1024*1000,
+        'volumeSize' => 1024 * 1000,
         'compression' => 0
     ])
 );
