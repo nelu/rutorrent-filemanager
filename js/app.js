145d4ed5747621b847ea3959c01c7d15d24e7fca
nelu <nelu@github.com>
2024-10-24 06:37:09 +0300

    dialog views updates and fixes for v5

--- js/app.js
+++ js/app.js
@@ -66,6 +66,8 @@ import {FileManagerUi} from "./ui.js";
         let self = this;
 
         self.inaction = false;
+        self.notification = null;
+
         self.cleanactions = () => {
 
             $(".fMan_Stop").attr('disabled', true);
@@ -79,6 +81,26 @@ import {FileManagerUi} from "./ui.js";
             theWebUI.FileManager.actionlp = 0;
 
         }
+
+        self.doDelete = function (paths) {
+
+            var deferred = $.Deferred();
+
+            if (!$type(paths) || paths.length === 0) {
+                deferred.reject({errcode: 'delete', msg: 'Empty paths'});
+                return deferred.promise();
+            }
+
+            paths = flm.getFullPaths(paths);
+            const cPath = flm.getCurrentPath();
+            flm.actions.notify(theUILang.fStarts.delete + ": " + paths.length + " files");
+
+            return flm.api.removeFiles(paths).then(function (result) {
+                flm.actions.refreshIfCurrentPath(cPath);
+                $(document).trigger(flm.EVENTS.delete, [paths]);
+                return result;
+            });
+        };
 
         self.logStop = () => {
 
@@ -118,6 +140,32 @@ import {FileManagerUi} from "./ui.js";
 
         self.logConsole = (action, text) => {
             flm.ui.console.logMsg(action + ': ' + text);
+        }
+
+        self.refreshIfCurrentPath = (path) => {
+            // refresh in case we are in path
+            if (!flm.utils.isDir(path)) {
+                // when destination is a directory name
+                path = flm.utils.basedir(path)
+            }
+
+            const same = (path === flm.getCurrentPath());
+            same && flm.Refresh()
+            return same;
+        }
+
+        self.notify = (contents, color = 'information', hideAfter= 5000) => {
+            self.notification = $.noty(
+                {
+                    text: contents,
+                    layout: 'bottomLeft',
+                    type: color,
+                    timeout: hideAfter,
+                    closeOnSelfClick: true
+                });
+
+//            flm.actions.logConsole('rename', source + ' -> ' + destination);
+
         }
 
         self.doMediainfo = (target) => {
